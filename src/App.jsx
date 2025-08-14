2875424313a3604918e2159a63eb18440ce4538b
cerro-largo-dev <cerrolargogobierno@gmail.com>
2025-08-13 22:09:33 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import MapComponent from './components/MapComponent';
 import AdminIndex from './pages/admin'; // Importar el index del panel de administración
 import './App.css';
