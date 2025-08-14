452373600759dea70b55701bbe5d2100988ba4e6
cerro-largo-dev <cerrolargogobierno@gmail.com>
2025-08-14 12:03:12 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import { AuthProvider } from '@/hooks/useAuth.jsx' // 👈 IMPORTANTE
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>        {/* 👈 ENVUELVE TODA LA APP */}
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
