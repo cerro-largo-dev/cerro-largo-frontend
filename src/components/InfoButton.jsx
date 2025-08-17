47d619dd5869846328f353cc1cba3bfae90e16e2
cerro-largo-dev <cerrolargogobierno@gmail.com>
2025-08-17 17:25:45 -0300

    Update InfoButton.jsx

--- src/components/InfoButton.jsx
+++ src/components/InfoButton.jsx
@@ -6,8 +6,8 @@ const InfoButton = forwardRef(({ onClick }, ref) => {
       ref={ref}
       onClick={onClick}
       className="w-14 h-14 bg-blue-400 hover:bg-blue-500 text-white rounded-full shadow-lg flex items-center justify-center focus:outline-none"
-      title="Información"
-      aria-label="Información"
+      title="Inicio / Información"
+      aria-label="Inicio / Información"
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -17,9 +17,8 @@ const InfoButton = forwardRef(({ onClick }, ref) => {
         stroke="currentColor"
         strokeWidth="2"
       >
-        <circle cx="12" cy="12" r="10" />
-        <path d="M12 16v-4" />
-        <path d="M12 8h.01" />
+        <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+        <polyline points="9,22 9,12 15,12 15,22" />
       </svg>
     </button>
   );
