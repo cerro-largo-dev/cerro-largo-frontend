31059ccf706e19875a108be45f58f986747d12d4
cerro-largo-dev <cerrolargogobierno@gmail.com>
2025-08-18 22:17:14 -0300

    Update ReportModal.jsx

--- src/components/Reportes/ReportModal.jsx
+++ src/components/Reportes/ReportModal.jsx
@@ -401,7 +401,7 @@ const ReportModal = ({ open, onClose, onLocationChange, anchorRect }) => {
                     Enviando...
                   </>
                 ) : (
-                  'Enviar Reporte'
+                  'Enviar'
                 )}
               </button>
             </div>
