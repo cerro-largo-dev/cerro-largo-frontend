ae5068471d103dce671732de11b90e6890fefe52
cerro-largo-dev <cerrolargogobierno@gmail.com>
2025-08-24 19:59:23 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,18 @@
-// vite.config.js — limpio y válido
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
-  },
+ plugins: [react(), tailwindcss()],
   build: {
-    sourcemap: false,
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ["react", "react-dom", "react-router-dom"],
-          map: ["leaflet", "react-leaflet"],
-        },
-      },
-    },
+          vendor: ['react','react-dom','react-router-dom'],
+          map: ['leaflet','react-leaflet']
+        }
+      }
+    }
   },
-});
