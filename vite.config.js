135277c3c83af19007ddfcdde5ebbdf4c002e586
cerro-largo-dev <cerrolargogobierno@gmail.com>
2025-08-24 19:52:43 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",                      // sirve en raíz
-  plugins: [react()],             // asegura JSX/React
-  define: { "process.env": {} },  // evita "process is not defined" en runtime
+  base: "/", // servir desde raíz
+  plugins: [react()],
+  define: { "process.env": {} }, // evita "process is not defined" en runtime
   build: {
-    target: "es2019",             // mejor compat Safari iOS 16.x
+    target: "es2019", // mejor compat iOS 16.x
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ["react","react-dom","react-router-dom"],
-          map: ["leaflet","react-leaflet"],
+          vendor: ["react", "react-dom", "react-router-dom"],
+          map: ["leaflet", "react-leaflet"],
         },
       },
     },
+  },
+  server: {
+    host: true,
+    port: 5173,
   },
 });
