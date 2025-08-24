b030206b4ad9f3aab9139ea3de51b135e1b142ed
cerro-largo-dev <cerrolargogobierno@gmail.com>
2025-08-24 19:54:37 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: "/", // servir desde raíz
-  plugins: [react()],
-  define: { "process.env": {} }, // evita "process is not defined" en runtime
-  build: {
-    target: "es2019", // mejor compat iOS 16.x
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom", "react-router-dom"],
-          map: ["leaflet", "react-leaflet"],
-        },
-      },
+  plugins: [react(),tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    host: true,
-    port: 5173,
-  },
-});
+})
